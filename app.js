3b679a201ab9a63e615765d4a8aaf0469a7ab54f
Anand Parthiban <33522459+ap5272@users.noreply.github.com>
2022-07-29 12:22:38 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -145,3 +145,5 @@ io.on('connection', function(socket) {
 http.listen(port, function() {
     console.log('listening on *: ' + port);
 });
+
+
--- public/connect.js
+++ public/connect.js
@@ -1,4 +1,3 @@
- 
   // THIS IS WHERE VIDEOCHAT IS ENABLED //
   
   
